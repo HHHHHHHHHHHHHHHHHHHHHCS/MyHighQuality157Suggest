01758be2eb2dfe0bfba1d1538652182da5b11887
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-12-10 17:27:56 +0800

    Initial commit

---
+++ MyHighQuality157Suggest/_001_正确的字符串.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHighQuality157Suggest
+{
+    public class _001_正确的字符串
+    {
+        public void Test01()
+        {
+            string s = "how" + 233;
+        }
+
+
+        public void Test02()
+        {
+            string s = "how" + 233.ToString();
+        }
+    }
+}
