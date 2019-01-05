200b637bb906bfe8466a41dd5d3aadd2377962f7
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-01-05 17:08:21 +0800

    035 036

---
+++ MyHighQuality157Suggest/_035_default初始T.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHighQuality157Suggest
+{
+    /// <summary>
+    /// 泛型的初始化用default
+    /// 有一些无参数结构体也可以用default
+    /// </summary>
+    public class _035_default初始T
+    {
+        public void Test01<T>(T t)
+        {
+            T temp = default(T);
+        }
+
+        public void Test02()
+        {
+            var p1 = default(Person);
+            Person p2 = default;
+            var p3 = default(P);
+            P p4 = default;
+        }
+
+        private class Person
+        {
+            public Person()
+            {
+
+            }
+        }
+
+        private struct P
+        {
+
+        }
+    }
+}
---
+++ MyHighQuality157Suggest/_036_FCL委托申明.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHighQuality157Suggest
+{
+    /// <summary>
+    /// 使用Action,Function,Predicate
+    /// Delegate至少0个参数，至多32个参数，可以无返回值，也可以指定返回值类型。这个是祖宗。
+    /// Func可以接受0个至16个传入参数，必须具有返回值。
+    /// Action可以接受0个至16个传入参数，无返回值。
+    /// Predicate只能接受一个传入参数，返回值为bool类型。
+    /// </summary>
+    public class _036_FCL委托申明
+    {
+        public delegate void Dg();
+        public event Dg evt; 
+
+        public void Test01()
+        {
+            evt += ()=>{ Console.WriteLine("Test01"); };
+        }
+
+        public void Test02()
+        {
+            Action act1 = () => { Console.WriteLine("Test02"); };
+            Action<int> act2 = (x) => { Console.WriteLine(x + "Test02"); };
+            act1();
+            act2(1);
+        }
+
+        public void Test03()
+        {
+            Func<int> func1 = () => { return 1; };
+            Func<int, int> func2 = (x) => { return 1 + x; };
+            Console.WriteLine(func1());
+            Console.WriteLine(func2(2));
+        }
+
+        public void Test04()
+        {
+            Predicate<int> pre1 = (x) => x > 100;
+            Console.WriteLine(pre1(1));
+        }
+    }
+}
